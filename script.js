c508b56f312fed2632b0d4de986999cf027762e8
alekcandr1 <alekcandrmain@gmail.com>
2023-12-09 22:43:45 +0300

    add links

--- script.js
+++ script.js
@@ -1,70 +1,40 @@
-// // Gallery
-// document.addEventListener('DOMContentLoaded', function () {
-//     let currentIndex = 0;
-//     const totalImages = document.querySelectorAll('.gallery-item').length;
+// Popup
+document.addEventListener("DOMContentLoaded", function () {
+    const openButtons = document.querySelectorAll(".open-popup-button");
+    const closeButtons = document.querySelectorAll(".close-button");
+    const overlay = document.getElementById("overlay");
 
-//     function showImage(index) {
-//         const images = document.querySelectorAll('.gallery-item');
-//         images.forEach(img => img.style.display = 'none');
-//         images[index].style.display = 'block';
-//     }
+    openButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            const popupId = this.getAttribute("data-popup-id");
+            const popup = document.getElementById(popupId);
 
-//     function changeImage(n) {
-//         currentIndex = (currentIndex + n + totalImages) % totalImages;
-//         showImage(currentIndex);
-//     }
+            if (popup) {
+                popup.classList.add("popup-show");
+                overlay.style.display = "block";
+            }
+        });
+    });
 
-//     document.getElementById('prevBtn').addEventListener('click', function () {
-//         changeImage(-1);
-//     });
+    closeButtons.forEach(function (closeButton) {
+        closeButton.addEventListener("click", function () {
+            const popupId = this.getAttribute("data-popup-id");
+            const popup = document.getElementById(popupId);
 
-//     document.getElementById('nextBtn').addEventListener('click', function () {
-//         changeImage(1);
-//     });
+            if (popup) {
+                popup.classList.remove("popup-show");
+                overlay.style.display = "none";
+            }
+        });
+    });
 
-//     // Show the initial image
-//     showImage(currentIndex);
-// });
- 
-
-
-// // Popup
-// document.addEventListener("DOMContentLoaded", function () {
-//     const openButtons = document.querySelectorAll(".open-popup-button");
-//     const closeButtons = document.querySelectorAll(".close-button");
-//     const overlay = document.getElementById("overlay");
-
-//     openButtons.forEach(function (button) {
-//         button.addEventListener("click", function () {
-//             const popupId = this.getAttribute("data-popup-id");
-//             const popup = document.getElementById(popupId);
-
-//             if (popup) {
-//                 popup.classList.add("popup-show");
-//                 overlay.style.display = "block";
-//             }
-//         });
-//     });
-
-//     closeButtons.forEach(function (closeButton) {
-//         closeButton.addEventListener("click", function () {
-//             const popupId = this.getAttribute("data-popup-id");
-//             const popup = document.getElementById(popupId);
-
-//             if (popup) {
-//                 popup.classList.remove("popup-show");
-//                 overlay.style.display = "none";
-//             }
-//         });
-//     });
-
-//     overlay.addEventListener("click", function (event) {
-//         const popups = document.querySelectorAll(".popup.popup-show");
-//         if (popups.length > 0 && event.target === overlay) {
-//             popups.forEach(function (popup) {
-//                 popup.classList.remove("popup-show");
-//             });
-//             overlay.style.display = "none";
-//         }
-//     });
-// });
+    overlay.addEventListener("click", function (event) {
+        const popups = document.querySelectorAll(".popup.popup-show");
+        if (popups.length > 0 && event.target === overlay) {
+            popups.forEach(function (popup) {
+                popup.classList.remove("popup-show");
+            });
+            overlay.style.display = "none";
+        }
+    });
+});
